1971211051b0cc8e055dd2ad22feabe5c473f781
Diego Chavarria <diego.chavarriap@gmail.com>
2021-01-30 08:36:59 -0500

    new operator added

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
 function add(a, b) {
-    return a + b;
+    return a / b;
 }
 
 function substract(a, b) {
     return a - b;
 }
+
 
 module.exports.add = add;
 module.exports.substract = substract;
